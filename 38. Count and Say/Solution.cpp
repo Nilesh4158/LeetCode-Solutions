b5d51d96f1bf22afe5b3d88ee1f163234b2010f6
Nilesh Bhardwaj <88051119+Nilesh4158@users.noreply.github.com>
2025-04-20 18:15:56 +0530

    C++ Solution
          Submission Statistics:
          Question #: 38
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 81.96
          Memory: 9.96 MB
          Memory percentile: 56.05

---
+++ 38. Count and Say/Solution.cpp
@@ -0,0 +1,25 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+        string prev = "1";
+        for (int i = 2; i <= n; ++i) {
+            string current;
+            int count = 1;
+            for (int j = 1; j < prev.size(); ++j) {
+                if (prev[j] == prev[j-1]) {
+                    ++count;
+                } else {
+                    current += to_string(count) + prev[j-1];
+                    count = 1;
+                }
+            }
+            current += to_string(count) + prev.back();
+            prev = current;
+        }
+        return prev;
+    }
+};
