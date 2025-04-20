434a1a2e9e3726976ade422978c46481505781ff
Nilesh Bhardwaj <88051119+Nilesh4158@users.noreply.github.com>
2025-04-20 18:18:17 +0530

    C++ Solution
          Submission Statistics:
          Question #: 40
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 14.088 MB
          Memory percentile: 61.58

---
+++ 40. Combination Sum II/Solution.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> current;
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, target, 0, current, result);
+        return result;
+    }
+
+private:
+    void backtrack(vector<int>& candidates, int target, int start, vector<int>& current, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = start; i < candidates.size(); ++i) {
+            if (candidates[i] > target) {
+                continue;
+            }
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            current.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], i + 1, current, result);
+            current.pop_back();
+        }
+    }
+};
