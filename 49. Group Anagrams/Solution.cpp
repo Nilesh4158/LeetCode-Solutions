088b88eadb463a7845e1769b3fcb80a38cf7b9a9
Nilesh Bhardwaj <88051119+Nilesh4158@users.noreply.github.com>
2025-04-22 19:26:45 +0530

    C++ Solution
          Submission Statistics:
          Question #: 49
          Language: C++
          Runtime: 11 ms
          Runtime percentile: 92.06
          Memory: 24.98 MB
          Memory percentile: 77.08

---
+++ 49. Group Anagrams/Solution.cpp
@@ -0,0 +1,21 @@
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        
+        for (const string& str : strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            anagramGroups[key].push_back(str);
+        }
+        
+        vector<vector<string>> result;
+        for (const auto& pair : anagramGroups) {
+            result.push_back(pair.second);
+        }
+        
+        return result;
+    }
+};
