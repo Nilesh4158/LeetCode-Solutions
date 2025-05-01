5785ef37035ecaabc10d86978fd85b274b1b759e
Nilesh Bhardwaj <88051119+Nilesh4158@users.noreply.github.com>
2025-05-01 18:09:45 +0530

    C++ Solution
          Submission Statistics:
          Question #: 79
          Language: C++
          Runtime: 196 ms
          Runtime percentile: 81.47
          Memory: 10.408 MB
          Memory percentile: 85.59

---
+++ 79. Word Search/Solution.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if (board.empty() || board[0].empty()) return false;
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+private:
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int index) {
+        if (index == word.size()) return true;
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[index]) {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#'; // Mark as visited
+        bool found = dfs(board, word, i + 1, j, index + 1) ||
+                     dfs(board, word, i - 1, j, index + 1) ||
+                     dfs(board, word, i, j + 1, index + 1) ||
+                     dfs(board, word, i, j - 1, index + 1);
+        board[i][j] = temp; // Revert back
+        return found;
+    }
+};
